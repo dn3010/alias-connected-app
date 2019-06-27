00d1e4595383fc948483b4e2360b40bfe25aec71
Scott Twiname <skott.twiname@gmail.com>
2019-06-27 13:21:07 +1200

    Add text input component

---
+++ src/base/textInput.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { TextInput as RNTextInput, StyleSheet } from 'react-native';
+import { colors } from '../constants';
+
+export default class TextInput extends Component {
+
+  render() {
+    return <RNTextInput {...this.props} style={[styles.input, this.props.style]}/>
+  }
+}
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'white',
+    borderRadius: 8,
+    fontSize: 16,
+    margin: 5,
+  }
+});
